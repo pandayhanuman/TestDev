6a85167a9fc44ceeb8b0a9dd777ebca99eedb31e
hanuman <panday.hanuman@gmail.com>
2015-04-18 21:56:57 +0530

    sdf

--- DevTest/src/com/man/test/Test.java
+++ DevTest/src/com/man/test/Test.java
@@ -4,9 +4,13 @@ public class Test {
 /* comment adding some comment */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< HEAD
         System.out.println("================================");
         System.out.println("Test.main()");
         
+=======
+
+>>>>>>> parent of a0fb8aa... modified by hanuman
 	}
 
 }
