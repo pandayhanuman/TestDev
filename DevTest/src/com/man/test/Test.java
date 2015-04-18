7d98c6bee48d42ab810a28db62836eb72a2597e9
hanuman <panday.hanuman@gmail.com>
2015-04-18 21:43:05 +0530

    add comment

--- DevTest/src/com/man/test/Test.java
+++ DevTest/src/com/man/test/Test.java
@@ -1,7 +1,7 @@
 package com.man.test;
 
 public class Test {
-
+/* comment adding some comment */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("================================");
